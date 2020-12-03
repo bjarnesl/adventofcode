14ae2c61094f2bb2e909cb3cc6719ac7156c21e0
Bjarne Veland Sletten <bjarnesl@gmail.com>
2020-12-03 09:17:43 +0100

    day3p2

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace day3
@@ -8,46 +9,98 @@ namespace day3
     {
 
         static readonly InputFetcher inputFetcher = new InputFetcher();
-        static int x = 0;
-        static int y = 0;
-        static int treesEncountered=0;
+        static int x;
+        static int xMover;
+        static int y;
+        static int yMover;
+        static uint treesEncountered;
+        static List<uint> treesEncounteredCol = new List<uint>();
         static  int maxWidth;
         static char treeIndicator ='#';
         static void Main(string[] args)
         {
             Console.WriteLine("Good morning World!");
 
+            SolvePart1();
+            SolvePart2();
+            Console.WriteLine("Done");
+        }
 
+        private static void SolvePart1()
+        {
             var originalInput = inputFetcher.GetInput(InputFetcher.day3p1);
             maxWidth = originalInput[0].Length;
-           while(y < originalInput.Length-1)
+            treesEncountered = 0;
+            x = 0;
+            y = 0;
+            xMover = 3;
+            yMover = 1;
+            while (y < originalInput.Length - 1)
             {
                 MoveForward();
                 var line = originalInput[y];
                 var chararray = line.ToCharArray();
                 var detected = chararray[x];
-                if(detected== treeIndicator)
+                if (detected == treeIndicator)
                 {
                     treesEncountered++;
                 }
             }
-            Console.WriteLine($"Trees: {treesEncountered}. OpenSlopes: {originalInput.Length-1 - treesEncountered}");
-            Console.WriteLine("Done");
+            Console.WriteLine($"Trees: {treesEncountered}. OpenSlopes: {originalInput.Length - 1 - treesEncountered}");
+        }
+
+        private static void SolvePart2()
+        {
+            var originalInput = inputFetcher.GetInput(InputFetcher.day3p1);
+            maxWidth = originalInput[0].Length;
+            var possibleMoves = new[]
+            {
+                new {movex =1, movey=1 },
+                new {movex =3, movey=1 },
+                new {movex =5, movey=1 },
+                new {movex =7, movey=1 },
+                new {movex =1, movey=2 }
+            };
+
+            foreach (var posmove in possibleMoves)
+            {
+                treesEncountered = 0;
+                x = 0;
+                y = 0;
+                xMover = posmove.movex;
+                yMover = posmove.movey;
+
+                while (y < originalInput.Length - 1)
+                {
+                    MoveForward();
+                    var line = originalInput[y];
+                    var chararray = line.ToCharArray();
+                    var detected = chararray[x];
+                    if (detected == treeIndicator)
+                    {
+                        treesEncountered++;
+                    }
+                }
+                treesEncounteredCol.Add(treesEncountered);
+               
+            }
+          
+            Console.WriteLine($"Trees: {treesEncounteredCol.Aggregate((uint)1,(x,y) => x*y)}. ");
         }
 
         private static void MoveForward()
         {
-            var proposedX = x + 3;
+            var proposedX = x + xMover;
             if (proposedX >= maxWidth)
             {
                 x = proposedX - maxWidth;
             }
             else
             {
-                x += 3;
+                x += xMover;
             }
             
-            y += 1;
+            y += yMover;
         }
     }
 }
